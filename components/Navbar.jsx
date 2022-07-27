3b6a7759cfb19b835918d6d2477cd69ddd80bdeb
YosmanProgramer <yosman.higuita028@gmail.com>
2022-07-27 16:33:13 -0500

    remove scroll navigation and navbar transition

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,25 +1,9 @@
-import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import CustomSeparatos from './CustomSeparator'
 
 export default function Navbar () {
-  const [scrollPosition, setScrollPosition] = useState(0)
-
-  const handleScroll = () => {
-    const position = window.pageYOffset
-    setScrollPosition(position)
-  }
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll, { passive: true })
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  }, [])
-
   return (
-    <header className={`fixed top-0 left-0 w-full pb-4 z-40 duration-300 ${scrollPosition > 0 ? 'pt-4 bg-gray-900 bg-opacity-100' : 'pt-[33.78px] bg-transparent bg-opacity-0'}`}>
+    <header className="absolute top-0 left-0 w-full pb-4 z-40 pt-[33.78px]">
       <div className='mx-auto w-10/12 max-w-[1300px] '>
         <div className='border-b border-white pb-[20.5px]'>
           <div className='flex justify-between'>
