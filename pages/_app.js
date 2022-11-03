b95bcdb2362d969403d6ba456b87cb5fa4f3584c
David Daza <03dazal@gmail.com>
2022-11-03 08:43:48 -0500

    Add GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { GoogleAnalytics } from 'nextjs-google-analytics'
 import { createContext, useState } from 'react'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
@@ -13,6 +14,10 @@ function MyApp ({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+      <GoogleAnalytics
+        trackPageViews
+        gaMeasurementId='UA-120994855-7'
+      />
       <Layout>
         <Component {...pageProps} />
       </Layout>
