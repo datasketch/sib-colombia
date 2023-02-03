7d1d0796809e42d0c34de337f919ca77581ec75b
DATASKETCH <datasketch@pop-os.localdomain>
2023-02-03 17:04:24 -0500

    Update for nabvar in mobile view

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,6 +15,9 @@ export default function Navbar () {
     }
     setChildrenRegions(children)
   }
+
+  const [menuIsActive, setMenuIsActive] = useState(false)
+
   const nav = [
     {
       label: 'Colombia',
@@ -123,7 +126,7 @@ export default function Navbar () {
             </div>
             <nav className='lg:self-end'>
               {/* DESKTOP */}
-              <ul className={'relative grid grid-cols-4 text-white gap-x-6'}>
+              <ul className={'hidden relative lg:grid lg:grid-cols-4 text-white gap-x-6'}>
                 {nav.map((item, i) =>
                   <DropDown key={i} >
                     <DropDown.Button className='font-lato text-sm' {...item} arrow={!!item.childs?.length}>
@@ -132,7 +135,6 @@ export default function Navbar () {
                     <DropDown.Items className='absolute top-[132%] bg-white w-40 flex flex-col gap-y-0.5 py-1.5 px-2.5'>
                       {item.childs?.map((el, key) =>
                         <>
-
                           <div key={el.label + key}>
                             <DropDown.Item className='text-black  py-1.5 hover:font-bold font-lato opacity-80 text-sm w-full flex justify-between' onClick={() => handleRegionsSelected(el.children)} color={item.color} href={el.href}>
                               {el.label}
@@ -151,6 +153,18 @@ export default function Navbar () {
                   </DropDown>
                 )}
               </ul>
+
+              {/* Movile */}
+              <ul className="bg-transparent border-gray-200 px-4 py-2.5 rounded dark:bg-gray-900 lg:hidden">
+              {/* <ul className={`lg:hidden fixed top-0 w-full h-screen bg-white z-40 py-20 px-10 text-2xl space-y-6 duration-500 ${menuIsActive ? 'right-0' : '-right-full'}`}> */}
+                <div className='container flex flex-wrap items-center justify-between mx-auto'>
+                  <button onClick={() => setMenuIsActive(false)} type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500  rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-expanded="false">
+                    <span className="sr-only">Open</span>
+                    <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"></path></svg>
+                  </button>
+                </div>
+              </ul>
+
             </nav>
           </div>
         </div>
