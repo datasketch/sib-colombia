65e7bdef8f4d12093b8be905e097b4300bafaae3
yoshi <yosman@datasketch.co>
2024-02-20 16:10:22 -0500

    update: add geo json format

--- components/MapDepartment.jsx
+++ components/MapDepartment.jsx
@@ -1,32 +1,51 @@
 import { ComposableMap, Geographies, Geography } from 'react-simple-maps'
 import boyacaJson from '../public/data/boyaca/boyaca2.json'
-/* import { useState, useEffect } from 'react' */
 
 const MapDepartment = () => {
-  if (boyacaJson && boyacaJson.territorio && boyacaJson.territorio.length > 0) {
-    boyacaJson.territorio.map(territorio => {
-      console.log('Coord')
-    })
+  const { territorio } = boyacaJson
+  const mapDataObj = territorio[0]
+  const mapDataCoords = mapDataObj.map_data
+
+  const geoJsonFormat = {
+    type: 'FeatureCollection',
+    features: mapDataCoords.reduce((prev, curr) => {
+      return [
+        ...prev,
+        {
+          type: 'Feature',
+          geometry: curr.geometry,
+          properties: {
+            id: curr.id,
+            name: curr.name,
+            depto: curr.depto,
+            label: curr.label,
+            n_especies: curr.n_especies,
+            n_registros: curr.n_registros
+          }
+        }
+      ]
+    }, [])
   }
 
   return (
     <>
-      <div style={{ width: '100%', height: '500px' }}>
-        <ComposableMap projection="geoMercator">
-          <Geographies geography={boyacaJson}>
+      <div className="border" style={{ width: '100%', height: '500px' }}>
+        <ComposableMap
+          projection="geoMercator"
+          projectionConfig={{
+            center: [-73.361944444444, -40.4541666666667],
+            scale: 0
+          }}
+        >
+          <Geographies geography={geoJsonFormat}>
             {({ geographies }) =>
-              geographies
-                .map((geo) => (
-                  <Geography
-                    key={geo.rsmKey}
-                    geography={geo}
-                   />
-                ))
+              geographies.map((geo) => (
+                <Geography key={geo.rsmKey} geography={geo} />
+              ))
             }
           </Geographies>
         </ComposableMap>
       </div>
-
     </>
   )
 }
